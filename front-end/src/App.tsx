085f54218ff48fbc65dc19809efc2c6bc6938f5c
jesusegg <jesusegg@hotmail.com>
2021-07-05 22:27:57 -0500

    jesus

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "./actions";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
