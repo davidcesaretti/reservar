f9f030e6b400856cbc83c3960bf8e5c0156e61c7
Dariovelazquez7 <dario.velazquez10@gmail.com>
2021-07-07 12:30:01 -0300

    Prueba con base de datos con un producto cargado

--- api/db.ts
+++ api/db.ts
@@ -1,11 +1,13 @@
-import mongoose from 'mongoose'
+const mongoose= require ("mongoose");
+const chalk= require("chalk");
+require('dotenv').config();
 
-const {DB_HOST, DB_NAME} = process.env
-const MONGODB_URL = `mongodb://${DB_HOST}/${DB_NAME}`
+const {DB_HOST, DB_NAME, DB_PORT} = process.env
+const MONGODB_URL = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
 
 mongoose.connect(MONGODB_URL, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
 })
-    .then(db => console.log('Database is connected'))
-    .catch(err => console.error(err));
+.then(() => console.log(chalk.blue(`Database is connected on ${MONGODB_URL}`)))
+.catch(err => console.error(err));
