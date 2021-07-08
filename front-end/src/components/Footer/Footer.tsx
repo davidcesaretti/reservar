52dfe26f82fc680f0d07cb5cdc332bebb656f1ef
ulises2125 <ulisesscardino@gmail.com>
2021-07-08 00:22:39 -0300

    footer/prueba

--- front-end/src/components/Footer/Footer.tsx
+++ front-end/src/components/Footer/Footer.tsx
@@ -2,36 +2,37 @@ import { Link } from '@material-ui/icons';
 import React from 'react';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import InstagramIcon from '@material-ui/icons/Instagram';
+import style from './Footer.module.css';
 
 
 const Footer = () => {
     return (
-        <div>
-            <div>
-                <h4>ACERCA DE</h4>
+        <div className={style.footer}>
+            <div className={style.acerca}>
+                <h4 style={{marginLeft: "38px"}}>ACERCA DE</h4>
                 <ul>
-                    <ul><a href="">¿CÓMO FUNCIONA?</a></ul>
-                    <ul><a href="">TÉRMINOS Y CONDICIONES</a></ul>
-                    <ul><a href="">MAPA DEL SITIO</a></ul>
-                    <ul><a href="">PREGUNTAS FRECUENTES</a></ul>
-                    <ul><a href="">POLÍTICA DE PRIVACIDAD</a></ul>
+                    <li><a href="">¿CÓMO FUNCIONA?</a></li>
+                    <li><a href="">TÉRMINOS Y CONDICIONES</a></li>
+                    <li><a href="">MAPA DEL SITIO</a></li>
+                    <li><a href="">PREGUNTAS FRECUENTES</a></li>
+                    <li><a href="">POLÍTICA DE PRIVACIDAD</a></li>
                 </ul>
             </div>
-            <div>
-                <h4>OPCIONES DE ALOJAMIENTO</h4>
+            <div className={style.ops}>
+                <h4 style={{marginLeft: "38px"}}>OPCIONES DE ALOJAMIENTO</h4>
                 <ul>
-                    <ul><a href="">HOSTALES/BED & BREAKFAST</a></ul>
-                    <ul><a href="">CASAS & DEPARTAMENTOS</a></ul>
-                    <ul><a href="">FUERA DE LO COMUN</a></ul>
-                    <ul><a href="">QUIERO SER HOST</a></ul>
+                    <li><a href="">HOSTALES/BED & BREAKFAST</a></li>
+                    <li><a href="">CASAS & DEPARTAMENTOS</a></li>
+                    <li><a href="">FUERA DE LO COMUN</a></li>
+                    <li><a href="">QUIERO SER HOST</a></li>
                 </ul>
             </div>
-            <div>
-                <h4>SÍGUENOS</h4>
-                <ul>
+            <div className={style.iconos}>
+                <h4 style={{marginLeft: "38px"}}>SÍGUENOS</h4>
+                <li>
                     <TwitterIcon/>
                     <InstagramIcon/>
-                </ul>
+                </li>
             </div>
         </div>
     )
