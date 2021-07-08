04b86489b413aeb7f20f87b50c43919fedcb7af2
Pevaq <vaquerp@gmail.com>
2021-07-08 15:47:03 -0300

    testing cloud

--- api/src/models/Properties.ts
+++ api/src/models/Properties.ts
@@ -1,16 +1,43 @@
-const {Schema, model} = require("mongoose")
+const { Schema, model } = require("mongoose");
 
 const Properties = new Schema({
-    name: {
-        type: String,
-},
-    price: {
-        type: Number,
-        default: 0
-    },
-    adress: {
-        type: String
-    }
-})
+  name: {
+    type: String,
+  },
+  summary: {
+    type: String,
+  },
+  type: {
+    type: String,
+  },
+  accommodates: {
+    type: Number,
+  },
+  beds: {
+    type: Number,
+  },
+  bedrooms: {
+    type: Number,
+  },
+  bathrooms: {
+    type: String,
+  },
 
-module.exports= model("Properties", Properties)
+  amenities: {
+    type: [String],
+  },
+
+  price: {
+    type: String,
+  },
+
+  image: {
+    type: String,
+  },
+
+  adress: {
+    type: String,
+  },
+});
+
+module.exports = model("Properties", Properties);
--- api/test.ts
+++ api/test.ts
@@ -1,17 +1,38 @@
+import { dataAirbnb } from "./db";
 require ("./db");
 const Property = require ("./src/models/Properties")
 
 
 export const createdTest = async () => {
 
-    const propertyTest = new Property ({
-        name: "hostal",
-        adress: "Av P. Sherman 42",
-        price: 54.999
-    })
-    
-   const productSave=  await propertyTest.save()
-    console.log(productSave)
+    dataAirbnb.find({})
+    .limit(100)
+    .then((data:any) => console.log(data[1].summary))
+
+    //         new Property({
+    //             name: x.name,
+    //             summary: x.summary,
+    //             type:x.property_type,
+
+
+
+    //         })
+            
+            
+            
+            
+            
+    //         ));
+
+
+
+
+    //     const propertyTest = new Property ({
+            
+    //     })
+        
+    // const productSave=  await propertyTest.save()
+    //     console.log(productSave)
 }
 
 // createdTest()
