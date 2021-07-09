a26fa14617360876c0084ef2b540f29cf5a03744
Dariovelazquez7 <dario.velazquez10@gmail.com>
2021-07-09 18:15:24 -0300

    add models/routes user & token

--- api/src/controllers/verifyToken.ts
+++ api/src/controllers/verifyToken.ts
@@ -1,16 +1,17 @@
-import express, { Request, Response, NextFunction, Application } from "express";
-const User = require("../models/Users")
-const jwt= require("jsonwebtoken")
+import jwt from "jsonwebtoken"
+
+
 const { SECRET_TOKEN} = process.env;
 
 
-export default function verifyToken (req, res, next){
+ function verifyToken (req, res, next){
     const token = req.headers["x-access-token"]
   if(!token){
-    return res.status(401).json({auth: false, message: "No token provided"})
+    return res.status(401).json({authorization: false, message: "No token provided, access denied"})
   }
   const decoded = jwt.verify(token,SECRET_TOKEN )
   req.userId = decoded.id
-  console.log(decoded)
   next()
 }
+
+export default verifyToken
