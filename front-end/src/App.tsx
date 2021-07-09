3d8b9f5b578efa40d3cdd3d87b20e9d7bd2231ed
Walter Cuberas <waltercuberas@MacBook-Air.local>
2021-07-09 12:30:20 -0300

    nuevo

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Login from './components/Login/Login'
 import Register from './components/Register/Register'
+import Home from './components/Home/Home'
 
 function App() {
   return (
     <Switch>
+      <Route exact path='/' component={Home} />
       <Route exact path='/login' component={Login} />
       <Route exact path='/register' component={Register} />
     </Switch>
