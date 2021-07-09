f5cb73bb2ad69fc4439dcf235c84fae8639fba00
davidcesaretti <davucesaretti@gmail.com>
2021-07-09 05:51:18 -0300

    login facebook

--- front-end/src/components/login/login.tsx
+++ front-end/src/components/login/login.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { onLogin } from '../../actions/index'
 import { GoogleLogin } from 'react-google-login'
+import FacebookLogin from 'react-facebook-login';
 
 const Login = () => {
 
@@ -27,6 +28,11 @@ const Login = () => {
         console.log(response.profileObj)
     }
 
+
+    const responseFacebook = (response) => {
+        console.log(response)
+    }
+
     return (
         <form onSubmit={(e) => { log(e) }}>
             <label placeholder="username">Username</label>
@@ -47,10 +53,18 @@ const Login = () => {
             {error.length > 0 && <p>{error}</p>}
             <GoogleLogin
                 clientId="283885012153-bsj812gn457gvnqnoq6u1p2oup9t1304.apps.googleusercontent.com"
-                buttonText="Iniciar Sesión"
+                buttonText="Iniciar Sesión con Google"
                 onSuccess={responseGoogle}
                 onFailure={responseGoogle}
                 cookiePolicy={'single_host_origin'}
+            />
+            <FacebookLogin
+                appId="516762732768499"
+                autoLoad={false}
+                fields="name,email,picture"
+                callback={responseFacebook}
+                textButton="Iniciar sesión"
+                icon="fa-facebook"
             />
         </form>
 
