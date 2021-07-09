36addaa3f74685bb40e45a7b29c064bb3ea31901
davidcesaretti <davucesaretti@gmail.com>
2021-07-09 05:07:33 -0300

    login google

--- front-end/src/components/login/login.tsx
+++ front-end/src/components/login/login.tsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react';
-import {onLogin} from '../../actions/index'
+import React, { useState } from 'react';
+import { onLogin } from '../../actions/index'
+import { GoogleLogin } from 'react-google-login'
 
 const Login = () => {
 
-    const [{username, password}, setCredentials] = useState({
+    const [{ username, password }, setCredentials] = useState({
         username: '',
         password: '',
     })
@@ -21,8 +22,13 @@ const Login = () => {
         }
     }
 
+    const responseGoogle = (response) => {
+        console.log(response)
+        console.log(response.profileObj)
+    }
+
     return (
-        <form onSubmit={(e) => {log(e)}}>
+        <form onSubmit={(e) => { log(e) }}>
             <label placeholder="username">Username</label>
             <input placeholder="Username" value={username} onChange={(e) => {
                 setCredentials({
@@ -37,8 +43,15 @@ const Login = () => {
                     password: e.target.value,
                 })
             }} />
-            <button type="submit">Login</button>
+            <button type="submit">Ingresar</button>
             {error.length > 0 && <p>{error}</p>}
+            <GoogleLogin
+                clientId="283885012153-bsj812gn457gvnqnoq6u1p2oup9t1304.apps.googleusercontent.com"
+                buttonText="Iniciar Sesión"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
         </form>
 
     )
