2b285220c5a021eb64513e1d7f2957d3c20ee373
jesusegg <jesusegg@hotmail.com>
2021-07-10 15:09:34 -0500

    back

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,6 +3,8 @@ import { Route, Switch } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Home from "./components/Home/Home";
+import Calendary from "./components/Calendary/Calendary";
+import Card from "./components/Card/Card";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
+      <Route exact path="/Calendary" component={Calendary} />
+      <Route exact path="/Card" component={Card} />
     </Switch>
   );
 }
