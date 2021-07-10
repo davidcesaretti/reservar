3ddab5b989877b7943e8c5e868140aefce831432
Dariovelazquez7 <dario.velazquez10@gmail.com>
2021-07-10 04:51:37 -0300

    calendary test

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import Login from './components/Login/Login'
 import Register from './components/Register/Register'
 import Home from './components/Home/Home'
+import Calendary from "./components/Calendary/Calendary"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route exact path='/' component={Home} />
       <Route exact path='/login' component={Login} />
       <Route exact path='/register' component={Register} />
+      <Route exact path="/calendary" component={Calendary}/>
     </Switch>
   );
 }
