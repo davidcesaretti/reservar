75f9bdafc2d080708a9dd97985255d922b82654c
ulises2125 <ulisesscardino@gmail.com>
2021-07-10 12:32:10 -0300

    statefiltros

--- front-end/src/reducers/reducers.ts
+++ front-end/src/reducers/reducers.ts
@@ -1,14 +1,19 @@
 import { combineReducers } from "redux";
 import { usersReducer } from "./users";
 import { fake } from "../actions";
+import { cardsHotel, hotelsReducer } from './hotels';
+
 
 export interface StoreState {
   users: fake[];
+  cardsHotel: cardsHotel[];
 }
-
+ 
 export const reducers = combineReducers<StoreState>({
   users: usersReducer,
+  cardsHotel: hotelsReducer
 });
+
 
 export default reducers;
 // interface stateI {
