d6c1928b072ffee175120b329493f38991276fc4
jesusegg <jesusegg@hotmail.com>
2021-07-10 14:45:20 -0500

    back

--- api/src/routes/get.ts
+++ api/src/routes/get.ts
@@ -1,5 +1,4 @@
 import express, { Response, Request, Router, NextFunction } from "express";
-import { any } from "sequelize/types/lib/operators";
 import { dataAirbnb } from "../../db";
 import { paginado } from "../paginado";
 // import cors from "cors";
@@ -9,23 +8,37 @@ const router = Router();
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
   let { amenities, price, type } = req.query;
   if (
+    req.query.accommodates ||
     req.query.amenities ||
     req.query.score ||
     req.query.price ||
     req.query.type
   ) {
+    let accommodates: any = req.query.accommodates;
+    accommodates ? (accommodates = parseInt(accommodates)) : undefined;
     let score: any = req.query.score;
     score ? (score = parseInt(score)) : undefined;
 
     let obj = {};
-    let array = [{ score: score }, { amenities: amenities }, { type: type }];
+    let array = [
+      { score: score },
+      { amenities: amenities },
+      { type: type },
+      { accommodates: accommodates },
+    ];
 
-    console.log(typeof Object.values(array[1])[0], "invento");
+    // console.log(typeof Object.values(array[1])[0], "invento");
     array = array.filter((x) => Object.values(x)[0] !== undefined);
-    console.log(array, "array filtrado");
+    // console.log(array, "array filtrado");
     array.map((x) => Object.assign(obj, x));
 
-    console.log(obj, "objeto logeado");
+    //  console.log(obj, "objeto logeado");
+
+    if (Array.isArray(req.query.amenities)) {
+      dataAirbnb
+        .find({ amenities: { $all: req.query.amenities } })
+        .then((data) => res.json(paginado(req, res, data)));
+    }
 
     if (price) {
       dataAirbnb
@@ -39,42 +52,6 @@ router.get("/", (req: Request, res: Response, next: NextFunction) => {
       .then((data: any) => res.json(paginado(req, res, data)))
       .catch((err) => console.error(err));
   }
-
-  //   if (req.query.type) {
-  //     dataAirbnb.find({ type: req.query.type }).then((data) => res.json(data));
-  //   }
-
-  //   if (req.query.amenities && req.query.score && req.query.price) {
-  //     let score: any = req.query.score;
-  //     score = parseInt(score);
-  //     dataAirbnb
-  //       .find({ score: score, amenities: req.query.amenities })
-  //       .then((data) => res.json(data));
-  //   }
-
-  //   if (req.query.amenities) {
-  //     if (Array.isArray(req.query.amenities)) {
-  //       dataAirbnb
-  //         .find({ amenities: { $all: req.query.amenities } })
-  //         .then((data) => res.json(paginado(req, res, data)));
-  //     }
-
-  //     dataAirbnb
-  //       .find({ amenities: req.query.amenities })
-  //       .then((data) => res.json(paginado(req, res, data)));
-  //   }
-
-  //   if (req.query.score) {
-  //     let score: any = req.query.score;
-  //     score = parseInt(score);
-  //     dataAirbnb.find({ score: score }).then((data) => res.json(data));
-  //   }
-  //   if (req.query.price) {
-  //     dataAirbnb
-  //       .find({})
-  //       .sort({ price: req.query.price })
-  //       .then((data) => res.json(data));
-  //   }
 });
 
 export default router;
