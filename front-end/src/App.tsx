1a48a4ea2419c01cdd3d8e002e572f54525f7fbd
Pevaq <vaquerp@gmail.com>
2021-07-12 02:09:32 -0300

    ahora si

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Login from "./components/Login/Login";
-import Register from "./components/Register/Register";
+import Register from "./components/Register/Register2";
 import Home from "./components/Home/Home";
 import Calendary from "./components/Calendary/Calendary";
 import Album from "./components/Cards/Cards";
