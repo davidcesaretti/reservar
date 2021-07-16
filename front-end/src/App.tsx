461f50e8545e90430072475ed18e66a73f58548c
davidcesaretti <davucesaretti@gmail.com>
2021-07-16 12:13:50 -0300

    firebase-context

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {useSelector} from 'react-redux'
 import { Route, Switch } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register2";
@@ -7,20 +8,22 @@ import Calendary from "./components/Calendary/Calendary";
 import Album from "./components/Cards/Cards";
 import Filters from "./components/Filters/Filters";
 import DetailHotel from "./components/DetailHotel/DetailHotel";
-
+import { useAuth } from "./firebase/index";
 import Perfil from './components/Perfil/Perfil';
 function App() {
+
+  const auth = useAuth();
+
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       
-      <Route exact path="/filters" component={Filters} />
       <Route exact path="/categories" component={Album} />
       <Route exact path="/categories/:id" component={DetailHotel}/>
       <Route exact path="/Login" component={Login} />
-      <Route exact path="/Register" component={Register} />
+      <Route exact path="/Register" component={auth.user ? Perfil : Register} />
       <Route exact path="/Calendary" component={Calendary} />
-      <Route exact path="/perfil" component={Perfil} />
+      <Route exact path="/perfil" component={auth.user ? Perfil : Register} />
       <Route exact path="/Filters" component={Filters} />
       <Route exact path="/Categories" component={Album} />
     </Switch>
