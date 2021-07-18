ce111aa9b3556c8fe1d2ab68579cad4b6b38ce6f
V33RO <cardozoveronicaro@gmail.com>
2021-07-18 19:45:06 -0300

    prubmerge

--- front-end/src/components/Payments/Reservation.tsx
+++ front-end/src/components/Payments/Reservation.tsx
@@ -1,16 +1,8 @@
 import React, { useEffect } from "react";
 import Card from "@material-ui/core/Card";
-import CardContent from '@material-ui/core/CardContent';
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import CardComp from "../CardComp/CardComp";
-import Footer from "../Footer/Footer";
-import NavBarPago from "../Nav/NavPago";
-import Recom1 from "../../Image/recom1.jpeg";
-import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
 //import * as moment from 'moment';
 
