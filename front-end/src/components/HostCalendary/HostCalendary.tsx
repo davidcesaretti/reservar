6cf3b5ca1cdb5dd046829b54ba8b6b283268fbc2
V33RO <cardozoveronicaro@gmail.com>
2021-07-23 09:54:20 -0300

    base

---
+++ front-end/src/components/HostCalendary/HostCalendary.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginTop: theme.spacing(2),
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    marginTop: theme.spacing(2),
+    flexGrow: 1,
+  },
+  link: {
+    marginTop: theme.spacing(2),
+    textDecoration: "none",
+    color: "white",
+    marginRight: theme.spacing(2),
+  },
+  link1: {
+    marginTop: theme.spacing(2),
+    textDecoration: "none",
+    color: "white",
+  },
+}));
+
+export default function HostCalendary() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      Host calendary
+    </div>
+  );
+}
