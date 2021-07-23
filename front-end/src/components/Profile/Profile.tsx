e47c08ec285977eff26c296f4e5413bd23aa6d0b
davidcesaretti <davucesaretti@gmail.com>
2021-07-23 10:38:54 -0300

    change between host and traveller

--- front-end/src/components/Profile/Profile.tsx
+++ front-end/src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import style from "./Profile.module.css";
 
 
 const Profile = (
-    {   name,
+    { name,
         alternative_email,
         phone_number,
         identity_document_type,
@@ -23,19 +23,21 @@ const Profile = (
     return (
         <div className={style.ctn}>
             <div className={style.information}>
-                <div>Name: {name}</div>
-                <div>Email: {auth.user.email}</div>
-                <div>Alternative Email: {alternative_email}</div>
-                <div>Phone Number: {phone_number}</div>
-                <div>Identity Document Type: {identity_document_type}</div>
-                <div>Identity Document Number: {identity_document_number}</div>
-                <div>Nationality: {nationality}</div>
-                <div>Birth Day: {date_birth?.slice(0, 10)}</div>
-                <div>Residence Address: {residence_address}</div>
-                <div>City and Country of Residence: {city_and_country_of_residence}</div>
-                <div>Emergency Phone Number: {emergency_phone_number}</div>
-                <div>Emergency Email: {emergency_contact}</div>
-                <div>Relationship of Contact: {relationship}</div>
+                <div className={style.user}>
+                    <div className={style.prop}><b>Name: </b>{name}</div>
+                    <div className={style.prop}><b>Email: </b>{auth.user.email}</div>
+                    <div className={style.prop}><b>Alternative Email: </b>{alternative_email}</div>
+                    <div className={style.prop}><b>Phone Number: </b>{phone_number}</div>
+                    <div className={style.prop}><b>Identity Document Type: </b>{identity_document_type}</div>
+                    <div className={style.prop}><b>Identity Document Number: </b>{identity_document_number}</div>
+                    <div className={style.prop}><b>Nationality: </b>{nationality}</div>
+                    <div className={style.prop}><b>Birth Day: </b>{date_birth?.slice(0, 10)}</div>
+                    <div className={style.prop}><b>Residence Address: </b>{residence_address}</div>
+                    <div className={style.prop}><b>City and Country of Residence: </b>{city_and_country_of_residence}</div>
+                    <div className={style.prop}><b>Emergency Phone Number: </b>{emergency_phone_number}</div>
+                    <div className={style.prop}><b>Emergency Email: </b>{emergency_contact}</div>
+                    <div className={style.prop}><b>Relationship of Contact: </b>{relationship}</div>
+                </div>
             </div>
             <div className={style.userType}>
                 {userType === "Traveller" ?
@@ -47,10 +49,10 @@ const Profile = (
                     </div>
                     :
                     <div>
-                        <div className={style.imgHost}>
+                        <div className={style.imgTravellers}>
                             <h1 className={style.title}>Go back to traveler panel?</h1>
                         </div>
-                        <button className={style.button} onClick={() => setUserType("Traveller")}>Traveller</button>
+                        <button className={style.buttonHost} onClick={() => setUserType("Traveller")}>TRAVELLER</button>
                     </div>
                 }
             </div>
