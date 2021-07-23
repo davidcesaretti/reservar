16d743bcc12b13896dcc04bc7a1cfae95b74d4a0
davidcesaretti <davucesaretti@gmail.com>
2021-07-23 10:38:54 -0300

    change between host and traveller

--- front-end/src/components/User/User.tsx
+++ front-end/src/components/User/User.tsx
@@ -56,7 +56,8 @@ const User = () => {
 
   const clickProfile = () => {
     /* if (userInfo.nationality !== undefined) { */
-      setSection("Profile");
+    setSection("Profile");
+    setSectionHost("Profile")
     /* } else {
       Swal.fire({
         title: "Please, complete your information",
@@ -111,262 +112,262 @@ const User = () => {
 
   return (
     userType === 'Traveller' ?
-    <div>
-      <div className={style.navBar}>
-        <div>
-          {auth.user.photoURL ? (
-            <img
-              className={style.picture}
-              src={auth.user.photoURL}
-              alt="profile"
-            />
-          ) : (
-            <img
-              className={style.picture}
-              src="https://w7.pngwing.com/pngs/154/803/png-transparent-computer-icons-user-profile-avatar-blue-heroes-window.png"
-              alt="profile"
-            />
-          )}
-        </div>
-        <div className={style.menu}>
-          <div className={style.title}>
-            {userInfo?.name ? (
-              <h4 className={style.bienvenida}>Bienvenido {userInfo.name}</h4>
+      <div>
+        <div className={style.navBar}>
+          <div>
+            {auth.user.photoURL ? (
+              <img
+                className={style.picture}
+                src={auth.user.photoURL}
+                alt="profile"
+              />
             ) : (
-              <h4 className={style.bienvenida}>
-                Bienvenido {auth.user.displayName}
-              </h4>
+              <img
+                className={style.picture}
+                src="https://w7.pngwing.com/pngs/154/803/png-transparent-computer-icons-user-profile-avatar-blue-heroes-window.png"
+                alt="profile"
+              />
             )}
-            <div className={style.buttonsNavBar}>
-              <Link to="/" className={style.navButton1}>
-                Home
-              </Link>
-              <div className={style.line}></div>
-              <Link to="/categories" className={style.navButton}>
-                Categories
-              </Link>
-            </div>
           </div>
-          <div className={style.separator}></div>
-          <nav className={style.options}>
-            <button
-              className={style.buttonOption}
-              onClick={(e) => {
-                clickProfile();
-              }}
-            >
-              Profile
-            </button>
-            <div className={style.line}></div>
-            <button
-              className={style.buttonOption}
-              onClick={(e) => {
-                clickUpdate();
-              }}
-            >
-              Edit profile
-            </button>
-            <div className={style.line}></div>
-            <button
-              className={style.buttonOption}
-              onClick={() => {
-                clickBookings();
-              }}
-            >
-              My bookings
-            </button>
-            <div className={style.line}></div>
-            <button
-              className={style.buttonOption}
-              onClick={() => {
-                clickFavourites();
-              }}
-            >
-              Favourites
-            </button>
-            <div className={style.line}></div>
-            <button
-              className={style.buttonOption}
-              onClick={() => {
-                clickHistoryTravels();
-              }}
-            >
-              History of travels
-            </button>
-            <div className={style.line}></div>
-            <button
-              className={style.buttonOption}
-              onClick={() => logOut()}
-            >
-              Signout
-            </button>
-          </nav>
+          <div className={style.menu}>
+            <div className={style.title}>
+              {userInfo?.name ? (
+                <h4 className={style.bienvenida}>Bienvenido {userInfo.name}</h4>
+              ) : (
+                <h4 className={style.bienvenida}>
+                  Bienvenido {auth.user.displayName}
+                </h4>
+              )}
+              <div className={style.buttonsNavBar}>
+                <Link to="/" className={style.navButton1}>
+                  Home
+                </Link>
+                <div className={style.line}></div>
+                <Link to="/categories" className={style.navButton}>
+                  Categories
+                </Link>
+              </div>
+            </div>
+            <div className={style.separator}></div>
+            <nav className={style.options}>
+              <button
+                className={style.buttonOption}
+                onClick={(e) => {
+                  clickProfile();
+                }}
+              >
+                Profile
+              </button>
+              <div className={style.line}></div>
+              <button
+                className={style.buttonOption}
+                onClick={(e) => {
+                  clickUpdate();
+                }}
+              >
+                Edit profile
+              </button>
+              <div className={style.line}></div>
+              <button
+                className={style.buttonOption}
+                onClick={() => {
+                  clickBookings();
+                }}
+              >
+                My bookings
+              </button>
+              <div className={style.line}></div>
+              <button
+                className={style.buttonOption}
+                onClick={() => {
+                  clickFavourites();
+                }}
+              >
+                Favourites
+              </button>
+              <div className={style.line}></div>
+              <button
+                className={style.buttonOption}
+                onClick={() => {
+                  clickHistoryTravels();
+                }}
+              >
+                History of travels
+              </button>
+              <div className={style.line}></div>
+              <button
+                className={style.buttonOption}
+                onClick={() => logOut()}
+              >
+                Signout
+              </button>
+            </nav>
+          </div>
+        </div>
+        {section === "Profile" ? (
+          <Profile
+            name={userInfo?.name}
+            alternative_email={userInfo?.alternative_email}
+            phone_number={userInfo?.phone_number}
+            identity_document_type={userInfo?.identity_document_type}
+            identity_document_number={userInfo?.identity_document_number}
+            nationality={userInfo?.nationality}
+            date_birth={userInfo?.date_birth}
+            residence_address={userInfo?.residence_address}
+            city_and_country_of_residence={
+              userInfo?.city_and_country_of_residence
+            }
+            emergency_phone_number={userInfo?.emergency_phone_number}
+            emergency_contact={userInfo?.emergency_contact}
+            relationship={userInfo?.relationship}
+            userType={userType}
+            setUserType={setUserType}
+          />
+        ) : section === "Update" ? (
+          <UpdateProfile />
+        ) : section === "Bookings" ? (
+          <Bookings />
+        ) : section === "Favorites" ? (
+          <Favorites />
+        ) : section === "History" ? (
+          <HistoryTravels />
+        ) : (
+          <Spinner />
+        )}
+        <div className={style.footer}>
+          <p className={style.infoFooter}>COPYRIGHT 2021</p>
         </div>
       </div>
-      {section === "Profile" ? (
-        <Profile
-          name={userInfo?.name}
-          alternative_email={userInfo?.alternative_email}
-          phone_number={userInfo?.phone_number}
-          identity_document_type={userInfo?.identity_document_type}
-          identity_document_number={userInfo?.identity_document_number}
-          nationality={userInfo?.nationality}
-          date_birth={userInfo?.date_birth}
-          residence_address={userInfo?.residence_address}
-          city_and_country_of_residence={
-            userInfo?.city_and_country_of_residence
-          }
-          emergency_phone_number={userInfo?.emergency_phone_number}
-          emergency_contact={userInfo?.emergency_contact}
-          relationship={userInfo?.relationship}
-          userType={userType}
-          setUserType={setUserType}
-        />
-      ) : section === "Update" ? (
-        <UpdateProfile />
-      ) : section === "Bookings" ? (
-        <Bookings />
-      ) : section === "Favorites" ? (
-        <Favorites />
-      ) : section === "History" ? (
-        <HistoryTravels />
-      ) : (
-        <Spinner />
-      )}
-      <div className={style.footer}>
-        <p className={style.infoFooter}>COPYRIGHT 2021</p>
-      </div>
-    </div>
-    :
-    <div>
-      <div className={style.navBarHost}>
-        <div>
-          {auth.user.photoURL ? (
-            <img
-              className={style.picture}
-              src={auth.user.photoURL}
-              alt="profile"
-            />
-          ) : (
-            <img
-              className={style.picture}
-              src="https://w7.pngwing.com/pngs/154/803/png-transparent-computer-icons-user-profile-avatar-blue-heroes-window.png"
-              alt="profile"
-            />
-          )}
-        </div>
-        <div className={style.menu}>
-          <div className={style.title}>
-            {userInfo?.name ? (
-              <h4 className={style.bienvenida}>Bienvenido {userInfo.name}</h4>
+      :
+      <div>
+        <div className={style.navBarHost}>
+          <div>
+            {auth.user.photoURL ? (
+              <img
+                className={style.picture}
+                src={auth.user.photoURL}
+                alt="profile"
+              />
             ) : (
-              <h4 className={style.bienvenida}>
-                Bienvenido {auth.user.displayName}
-              </h4>
+              <img
+                className={style.picture}
+                src="https://w7.pngwing.com/pngs/154/803/png-transparent-computer-icons-user-profile-avatar-blue-heroes-window.png"
+                alt="profile"
+              />
             )}
-            <div className={style.buttonsNavBar}>
-              <Link to="/" className={style.navButton1}>
-                Home
-              </Link>
-              <div className={style.line}></div>
-              <Link to="/categories" className={style.navButton}>
-                Categories
-              </Link>
-            </div>
           </div>
-          <div className={style.separator}></div>
-          <nav className={style.options}>
-            <button
-              className={style.buttonOption}
-              onClick={(e) => {
-                clickProfile();
-              }}
-            >
-              Profile
-            </button>
-            <div className={style.line}></div>
-            <button
-              className={style.buttonOption}
-              onClick={(e) => {
-                clickCreate();
-              }}
-            >
-              Create Publications
-            </button>
-            <div className={style.line}></div>
-            <button
-              className={style.buttonOption}
-              onClick={() => {
-                clickProperties();
-              }}
-            >
-              My Properties
-            </button>
-            <div className={style.line}></div>
-            <button
-              className={style.buttonOption}
-              onClick={() => {
-                clickBalance();
-              }}
-            >
-              Balance
-            </button>
-            <div className={style.line}></div>
-            <button
-              className={style.buttonOption}
-              onClick={() => {
-                clickChat();
-              }}
-            >
-              Chat
-            </button>
-            <div className={style.line}></div>
-            <button
-              className={style.buttonOption}
-              onClick={() => logOut()}
-            >
-              Signout
-            </button>
-          </nav>
+          <div className={style.menu}>
+            <div className={style.title}>
+              {userInfo?.name ? (
+                <h4 className={style.bienvenida}>Bienvenido {userInfo.name}</h4>
+              ) : (
+                <h4 className={style.bienvenida}>
+                  Bienvenido {auth.user.displayName}
+                </h4>
+              )}
+              <div className={style.buttonsNavBar}>
+                <Link to="/" className={style.navButton1}>
+                  Home
+                </Link>
+                <div className={style.line}></div>
+                <Link to="/categories" className={style.navButton}>
+                  Categories
+                </Link>
+              </div>
+            </div>
+            <div className={style.separator}></div>
+            <nav className={style.options}>
+              <button
+                className={style.buttonOption}
+                onClick={(e) => {
+                  clickProfile();
+                }}
+              >
+                Profile
+              </button>
+              <div className={style.line}></div>
+              <button
+                className={style.buttonOption}
+                onClick={(e) => {
+                  clickCreate();
+                }}
+              >
+                Create Publications
+              </button>
+              <div className={style.line}></div>
+              <button
+                className={style.buttonOption}
+                onClick={() => {
+                  clickProperties();
+                }}
+              >
+                My Properties
+              </button>
+              <div className={style.line}></div>
+              <button
+                className={style.buttonOption}
+                onClick={() => {
+                  clickBalance();
+                }}
+              >
+                Balance
+              </button>
+              <div className={style.line}></div>
+              <button
+                className={style.buttonOption}
+                onClick={() => {
+                  clickChat();
+                }}
+              >
+                Chat
+              </button>
+              <div className={style.line}></div>
+              <button
+                className={style.buttonOption}
+                onClick={() => logOut()}
+              >
+                Signout
+              </button>
+            </nav>
+          </div>
+        </div>
+
+        {sectionHost === "Profile" ? (
+          <Profile
+            name={userInfo?.name}
+            alternative_email={userInfo?.alternative_email}
+            phone_number={userInfo?.phone_number}
+            identity_document_type={userInfo?.identity_document_type}
+            identity_document_number={userInfo?.identity_document_number}
+            nationality={userInfo?.nationality}
+            date_birth={userInfo?.date_birth}
+            residence_address={userInfo?.residence_address}
+            city_and_country_of_residence={
+              userInfo?.city_and_country_of_residence
+            }
+            emergency_phone_number={userInfo?.emergency_phone_number}
+            emergency_contact={userInfo?.emergency_contact}
+            relationship={userInfo?.relationship}
+            userType={userType}
+            setUserType={setUserType}
+          />
+        ) : sectionHost === "CreatePublications" ? (
+          <CreatePublications />
+        ) : sectionHost === "MyProperties" ? (
+          <MyProperties />
+        ) : sectionHost === "Balance" ? (
+          <Balance />
+        ) : sectionHost === "Chat" ? (
+          <Chat />
+        ) : (
+          <Spinner />
+        )}
+
+
+        <div className={style.footerHost}>
+          <p className={style.infoFooter}>COPYRIGHT 2021</p>
         </div>
       </div>
-
-      {sectionHost === "Profile" ? (
-        <Profile
-          name={userInfo?.name}
-          alternative_email={userInfo?.alternative_email}
-          phone_number={userInfo?.phone_number}
-          identity_document_type={userInfo?.identity_document_type}
-          identity_document_number={userInfo?.identity_document_number}
-          nationality={userInfo?.nationality}
-          date_birth={userInfo?.date_birth}
-          residence_address={userInfo?.residence_address}
-          city_and_country_of_residence={
-            userInfo?.city_and_country_of_residence
-          }
-          emergency_phone_number={userInfo?.emergency_phone_number}
-          emergency_contact={userInfo?.emergency_contact}
-          relationship={userInfo?.relationship}
-          userType={userType}
-          setUserType={setUserType}
-        />
-      ) : sectionHost === "CreatePublications" ? (
-        <CreatePublications />
-      ) : sectionHost === "MyProperties" ? (
-        <MyProperties />
-      ) : sectionHost === "Balance" ? (
-        <Balance />
-      ) : sectionHost === "Chat" ? (
-        <Chat />
-      ) : (
-        <Spinner />
-      )}
-
-
-      <div className={style.footerHost}>
-        <p className={style.infoFooter}>COPYRIGHT 2021</p>
-      </div>
-    </div>
   )
 
 };
