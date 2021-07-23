5f192e63b473fdfdd7519a056fd0fd4addaceca0
davidcesaretti <davucesaretti@gmail.com>
2021-07-23 09:49:40 -0300

    host profile

--- front-end/src/components/Profile/Profile.tsx
+++ front-end/src/components/Profile/Profile.tsx
@@ -1,24 +1,59 @@
 import React from 'react'
 import { useAuth } from "../../firebase/index";
+import style from "./Profile.module.css";
 
-const Profile = (userInfo) => {
+
+const Profile = (
+    {   name,
+        alternative_email,
+        phone_number,
+        identity_document_type,
+        identity_document_number,
+        nationality,
+        date_birth,
+        residence_address,
+        city_and_country_of_residence,
+        emergency_phone_number,
+        emergency_contact,
+        relationship,
+        userType,
+        setUserType }
+) => {
     const auth = useAuth()
-    console.log('profile component ',userInfo)
     return (
-        <div>
-            <div>Name: {userInfo.name}</div>
-            <div>Email: {auth.user.email}</div>
-            <div>Alternative Email: {userInfo.alternative_email}</div>
-            <div>Phone Number: {userInfo.phone_number}</div>
-            <div>Identity Document Type: {userInfo.identity_document_type}</div>
-            <div>Identity Document Number: {userInfo.identity_document_number}</div>
-            <div>Nationality: {userInfo.nationality}</div>
-            <div>Birth Day: {userInfo.date_birth?.slice(0, 10)}</div>
-            <div>Residence Address: {userInfo.residence_address}</div>
-            <div>City and Country of Residence: {userInfo.city_and_country_of_residence}</div>
-            <div>Emergency Phone Number: {userInfo.emergency_phone_number}</div>
-            <div>Emergency Email: {userInfo.emergency_contact}</div>
-            <div>Relationship of Contact: {userInfo.relationship}</div>
+        <div className={style.ctn}>
+            <div className={style.information}>
+                <div>Name: {name}</div>
+                <div>Email: {auth.user.email}</div>
+                <div>Alternative Email: {alternative_email}</div>
+                <div>Phone Number: {phone_number}</div>
+                <div>Identity Document Type: {identity_document_type}</div>
+                <div>Identity Document Number: {identity_document_number}</div>
+                <div>Nationality: {nationality}</div>
+                <div>Birth Day: {date_birth?.slice(0, 10)}</div>
+                <div>Residence Address: {residence_address}</div>
+                <div>City and Country of Residence: {city_and_country_of_residence}</div>
+                <div>Emergency Phone Number: {emergency_phone_number}</div>
+                <div>Emergency Email: {emergency_contact}</div>
+                <div>Relationship of Contact: {relationship}</div>
+            </div>
+            <div className={style.userType}>
+                {userType === "Traveller" ?
+                    <div>
+                        <div className={style.imgHost}>
+                            <h1 className={style.title}>Do you want to access the host panel?</h1>
+                        </div>
+                        <button className={style.button} onClick={() => setUserType("Host")}>HOST</button>
+                    </div>
+                    :
+                    <div>
+                        <div className={style.imgHost}>
+                            <h1 className={style.title}>Go back to traveler panel?</h1>
+                        </div>
+                        <button className={style.button} onClick={() => setUserType("Traveller")}>Traveller</button>
+                    </div>
+                }
+            </div>
         </div>
     )
 }
--- front-end/src/components/User/User.tsx
+++ front-end/src/components/User/User.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useAuth } from "../../firebase/index";
 import style from "./User.module.css";
 import Profile from "../Profile/Profile";
@@ -9,6 +9,10 @@ import Bookings from "../Bookings/Bookings";
 import Favorites from "../Favorites/Favorites";
 import Spinner from "../Spinner/Spinner";
 import HistoryTravels from "../HistoryTravels/HistoryTravels";
+import CreatePublications from '../CreatePublications/CreatePublications'
+import MyProperties from '../MyProperties/MyProperties'
+import Balance from '../Balance/Balance'
+import Chat from '../Chat/Chat'
 import { getUserInfo } from "../../actions/index";
 import Swal from "sweetalert2";
 
@@ -16,9 +20,13 @@ const User = () => {
   const auth = useAuth();
 
   const dispatch = useDispatch();
+  const history = useHistory();
   let userInfo = useSelector((state: any) => state.user);
 
   const [section, setSection] = useState("");
+  const [userType, setUserType] = useState('Traveller')
+  const [sectionHost, setSectionHost] = useState('Profile')
+
   let email = auth.user.email;
   console.log("useSelector ", userInfo);
 
@@ -47,9 +55,9 @@ const User = () => {
   }
 
   const clickProfile = () => {
-    if (userInfo.nationality !== undefined) {
+    /* if (userInfo.nationality !== undefined) { */
       setSection("Profile");
-    } else {
+    /* } else {
       Swal.fire({
         title: "Please, complete your information",
         showClass: {
@@ -60,7 +68,7 @@ const User = () => {
         },
       });
       setSection("Update");
-    }
+    } */
   };
 
   const clickUpdate = () => {
@@ -79,7 +87,30 @@ const User = () => {
     setSection("History");
   };
 
+  const clickCreate = () => {
+    setSectionHost("CreatePublications");
+  };
+
+  const clickProperties = () => {
+    setSectionHost("MyProperties")
+  }
+
+  const clickBalance = () => {
+    setSectionHost("Balance")
+  }
+
+  const clickChat = () => {
+    setSectionHost("Chat")
+  }
+
+
+  const logOut = () => {
+    auth.signout()
+    history.push('/')
+  }
+
   return (
+    userType === 'Traveller' ?
     <div>
       <div className={style.navBar}>
         <div>
@@ -165,7 +196,7 @@ const User = () => {
             <div className={style.line}></div>
             <button
               className={style.buttonOption}
-              onClick={() => auth.signout()}
+              onClick={() => logOut()}
             >
               Signout
             </button>
@@ -188,6 +219,8 @@ const User = () => {
           emergency_phone_number={userInfo?.emergency_phone_number}
           emergency_contact={userInfo?.emergency_contact}
           relationship={userInfo?.relationship}
+          userType={userType}
+          setUserType={setUserType}
         />
       ) : section === "Update" ? (
         <UpdateProfile />
@@ -204,7 +237,138 @@ const User = () => {
         <p className={style.infoFooter}>COPYRIGHT 2021</p>
       </div>
     </div>
-  );
+    :
+    <div>
+      <div className={style.navBarHost}>
+        <div>
+          {auth.user.photoURL ? (
+            <img
+              className={style.picture}
+              src={auth.user.photoURL}
+              alt="profile"
+            />
+          ) : (
+            <img
+              className={style.picture}
+              src="https://w7.pngwing.com/pngs/154/803/png-transparent-computer-icons-user-profile-avatar-blue-heroes-window.png"
+              alt="profile"
+            />
+          )}
+        </div>
+        <div className={style.menu}>
+          <div className={style.title}>
+            {userInfo?.name ? (
+              <h4 className={style.bienvenida}>Bienvenido {userInfo.name}</h4>
+            ) : (
+              <h4 className={style.bienvenida}>
+                Bienvenido {auth.user.displayName}
+              </h4>
+            )}
+            <div className={style.buttonsNavBar}>
+              <Link to="/" className={style.navButton1}>
+                Home
+              </Link>
+              <div className={style.line}></div>
+              <Link to="/categories" className={style.navButton}>
+                Categories
+              </Link>
+            </div>
+          </div>
+          <div className={style.separator}></div>
+          <nav className={style.options}>
+            <button
+              className={style.buttonOption}
+              onClick={(e) => {
+                clickProfile();
+              }}
+            >
+              Profile
+            </button>
+            <div className={style.line}></div>
+            <button
+              className={style.buttonOption}
+              onClick={(e) => {
+                clickCreate();
+              }}
+            >
+              Create Publications
+            </button>
+            <div className={style.line}></div>
+            <button
+              className={style.buttonOption}
+              onClick={() => {
+                clickProperties();
+              }}
+            >
+              My Properties
+            </button>
+            <div className={style.line}></div>
+            <button
+              className={style.buttonOption}
+              onClick={() => {
+                clickBalance();
+              }}
+            >
+              Balance
+            </button>
+            <div className={style.line}></div>
+            <button
+              className={style.buttonOption}
+              onClick={() => {
+                clickChat();
+              }}
+            >
+              Chat
+            </button>
+            <div className={style.line}></div>
+            <button
+              className={style.buttonOption}
+              onClick={() => logOut()}
+            >
+              Signout
+            </button>
+          </nav>
+        </div>
+      </div>
+
+      {sectionHost === "Profile" ? (
+        <Profile
+          name={userInfo?.name}
+          alternative_email={userInfo?.alternative_email}
+          phone_number={userInfo?.phone_number}
+          identity_document_type={userInfo?.identity_document_type}
+          identity_document_number={userInfo?.identity_document_number}
+          nationality={userInfo?.nationality}
+          date_birth={userInfo?.date_birth}
+          residence_address={userInfo?.residence_address}
+          city_and_country_of_residence={
+            userInfo?.city_and_country_of_residence
+          }
+          emergency_phone_number={userInfo?.emergency_phone_number}
+          emergency_contact={userInfo?.emergency_contact}
+          relationship={userInfo?.relationship}
+          userType={userType}
+          setUserType={setUserType}
+        />
+      ) : sectionHost === "CreatePublications" ? (
+        <CreatePublications />
+      ) : sectionHost === "MyProperties" ? (
+        <MyProperties />
+      ) : sectionHost === "Balance" ? (
+        <Balance />
+      ) : sectionHost === "Chat" ? (
+        <Chat />
+      ) : (
+        <Spinner />
+      )}
+
+
+      <div className={style.footerHost}>
+        <p className={style.infoFooter}>COPYRIGHT 2021</p>
+      </div>
+    </div>
+  )
+
 };
 
 export default User;
