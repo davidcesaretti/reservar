6d47da29c3d29ea54f6c6121ac74b8e2eb85e228
Pevaq <vaquerp@gmail.com>
2021-07-23 19:52:31 -0300

    falta

--- front-end/src/actions/types.ts
+++ front-end/src/actions/types.ts
@@ -1,19 +1,19 @@
 import { FetchUsersAction } from "./index";
 
 export enum ActionTypes {
-  fetchUsers,
-  deleteUsers,
-  fetchCardsHotels,
-  signUser,
-  usersLogged,
-  detailHotel,
-  calendary,
-  addFav,
-  booleanState,
-  updateUser,
-  favUser,
-  bookings,
-  stateRegister
+  fetchUsers = "FETCH_USERS",
+  deleteUsers = "DELETE_USERS",
+  fetchCardsHotels = "FETCH_CARDS_HOTELS",
+  signUser = "SIGN_USERS",
+  usersLogged = "USERS_LOGGED",
+  detailHotel = "DETAIL_HOTEL",
+  calendary = "CALENDARY",
+  addFav = "ADD_FAV",
+  booleanState = "BOOLEAN_STATE",
+  updateUser = "UPDATE_USER",
+  favUser = "FAV_USER",
+  bookings = "BOOKINGS",
+  stateRegister = "STATE_REGISTER",
 }
 
 export type Action = FetchUsersAction;
--- front-end/src/reducers/reserves.ts
+++ front-end/src/reducers/reserves.ts
@@ -1,13 +1,13 @@
 import { Bookings } from "../actions/index";
 import { ActionTypes } from "../actions/types";
 
-
-
-export const reservesReducer = (state: Object= {}, action: Bookings) => {
+export const reservesReducer = (
+  state: Array<Object> = [],
+  action: Bookings
+) => {
   switch (action.type) {
     case ActionTypes.bookings:
-      console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaa")
-      return action.payload
+      return action.payload;
     default:
       return state;
   }
