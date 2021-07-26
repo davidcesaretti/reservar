cabd26222477f123e9dd5adc349929637803c114
Pevaq <vaquerp@gmail.com>
2021-07-26 11:42:36 -0300

    group chat

--- front-end/src/components/Nav/Nav2.tsx
+++ front-end/src/components/Nav/Nav2.tsx
@@ -54,8 +54,8 @@ export default function MenuAppBar() {
               Trekker Logo
             </Link>
           </Typography>
-          <Link to="/userpost" className={classes.link}>
-            <button>GO TO MY POSTS </button>
+          <Link to="/chat" className={classes.link}>
+            <button>GO TO MY CHAT </button>
           </Link>
           <Link to="/categories" className={classes.link}>
             <Button variant="contained" color="secondary">
