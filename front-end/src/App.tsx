a89d024f71bf74961bd4b8ced180745f4053b2ce
V33RO <cardozoveronicaro@gmail.com>
2021-07-28 13:59:56 -0300

    faq

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -15,6 +15,7 @@ import Favorites from "./components/Favorites/Favorites";
 import AddProperty from "./components/AddProperty/AddProperty";
 import AdminDashboard from "./components/AdminDashboard/AdminDashboard"
 import ValidationAdmin from "./components/ValidationAdmin/ValidationAdmin"
+import FAQ from "./components/FAQ/FAQ";
 
 
 function App() {
@@ -38,6 +39,7 @@ function App() {
       <Route exact path="/AddProperty/:id" component={AddProperty} />
       <Route exact path="/Admin" component={AdminDashboard} />
       <Route exact path="/validationAdmin" component={ValidationAdmin} />
+      <Route exact path="/prueba" component={FAQ} />
     </Switch>
   );
 }
--- front-end/src/components/TermsAndConditions/TermsAndConditions.tsx
+++ front-end/src/components/TermsAndConditions/TermsAndConditions.tsx
@@ -1,3 +1,4 @@
+import ActionViewArray from 'material-ui/svg-icons/action/view-array';
 import React from 'react'
 import style from './TermsAndConditions.module.css'
 
@@ -5,8 +6,9 @@ const TermsAndConditions = ({ setSection }) => {
 
     function save(ev){
         var newValue=ev.target.textContent;
-
+        console.log("termino",newValue)
       }
+    
     return (
         <div>
             <button onClick={() => { setSection('') }}>Back</button>
@@ -104,7 +106,7 @@ const TermsAndConditions = ({ setSection }) => {
                     <p className={style.final}>These <a href="https://policymaker.io/terms-and-conditions/">Terms of Service</a> were created for <b>trekker.com</b> by <a href="https://policymaker.io">PolicyMaker.io</a> on 2023-05-17.</p>
 
                </p>
-                <div className={style.botoncta}><p>Update</p></div>
+                <input type="button"  className={style.botoncta}><p className={style.botonup}>Update</p></input>
             </div>
         </div>
     )
