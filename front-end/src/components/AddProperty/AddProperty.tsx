46efc46f31a6409718b7d7b47503d88c79eba24f
jesusegg <jesusegg@hotmail.com>
2021-07-29 15:31:51 -0500

    fix

--- front-end/src/components/AddProperty/AddProperty.tsx
+++ front-end/src/components/AddProperty/AddProperty.tsx
@@ -16,6 +16,7 @@ import moment from "moment";
 import subDays from "date-fns/subDays";
 import HostCalendary from "../HostCalendary/HostCalendary";
 import SimpleModal from "./modal";
+import Swal from "sweetalert2";
 import NavBar from "../Nav/Nav2";
 
 function AddProperty() {
@@ -142,6 +143,9 @@ function AddProperty() {
       }
     );
   };
+
+
+
   const onFormSubmit = function (e) {
     e.preventDefault();
 
@@ -156,48 +160,65 @@ function AddProperty() {
     ) {
       alert("Please complete all the fields correctly");
     } else {
-      const formData = {
-        name: refTitle.current.value,
-        summary: refDescription.current.value,
-        type: refType.current.value,
-        accommodates: guests,
-        beds: beds,
-        bedrooms: bedrooms,
-        bathrooms: bathrooms,
-        amenities: amenities,
-        price: refPrice.current.value,
-        image: firebaseStorage.picture,
-        address: refAddress.current.value + " " + refCountry.current.value,
-        city: refCity.current.value,
-        score: 0,
-        host: auth,
-        coordinates: maps,
-      };
-      axios
-        .post("http://localhost:3001/upload", formData)
-        .then()
-        .catch((error) => console.log(error));
-      alert("The file is successfully uploaded");
-      [
-        refTitle.current.value,
-        refDescription.current.value,
-        refType.current.value,
-        refPrice.current.value,
-        firebaseStorage.picture,
-        refAddress.current.value,
-        refCity.current.value,
-        refCountry.current.value,
-      ] = ["", "", "", "", "", "", "", ""];
-      setAmenities([]);
-      firebaseStorage.uploadValue = 0;
-      const check: any = document.querySelectorAll("input[type='checkbox']");
-      check.forEach((x) => {
-        x.checked = false;
+      
+      Swal.fire({
+        title: "Do you want to create this property ?",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: `Accept`,
+        // denyButtonText: `Cancel`,
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          // axios.get(`http://localhost:3001/upload/delete/${_id}`);
+          const formData = {
+            name: refTitle.current.value,
+            summary: refDescription.current.value,
+            type: refType.current.value,
+            accommodates: guests,
+            beds: beds,
+            bedrooms: bedrooms,
+            bathrooms: bathrooms,
+            amenities: amenities,
+            price: refPrice.current.value,
+            image: firebaseStorage.picture,
+            address: refAddress.current.value + " " + refCountry.current.value,
+            city: refCity.current.value,
+            score: 0,
+            host: auth,
+            coordinates: maps,
+          };
+          axios
+          .post("http://localhost:3001/upload", formData)
+          .then()
+          .catch((error) => console.log(error));
+        [
+          refTitle.current.value,
+          refDescription.current.value,
+          refType.current.value,
+          refPrice.current.value,
+          firebaseStorage.picture,
+          refAddress.current.value,
+          refCity.current.value,
+          refCountry.current.value,
+        ] = ["", "", "", "", "", "", "", ""];
+        setAmenities([]);
+        firebaseStorage.uploadValue = 0;
+        const check: any = document.querySelectorAll("input[type='checkbox']");
+        check.forEach((x) => {
+          x.checked = false;
+        });
+        setGuests(1);
+        setBathrooms(1);
+        setBeds(1);
+        setBedrooms(1);
+          Swal.fire("Created!", "", "success");
+          // setTimeout(dispatchuser, 2000);
+        } else if (result.isDenied) {
+          Swal.fire("Property not created", "", "info");
+        }
       });
-      setGuests(1);
-      setBathrooms(1);
-      setBeds(1);
-      setBedrooms(1);
+     
     }
   };
 
@@ -215,6 +236,16 @@ function AddProperty() {
     ) {
       alert("Please complete all the fields correctly");
     } else {
+      Swal.fire({
+        title: "Do you want to edit this property ?",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: `Accept`,
+        // denyButtonText: `Cancel`,
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          // axios.get(`http://localhost:3001/upload/delete/${_id}`);
       const formData = {
         name: refTitle.current.value,
         summary: refDescription.current.value,
@@ -236,7 +267,6 @@ function AddProperty() {
         .post("http://localhost:3001/upload/edit", formData)
         .then()
         .catch((error) => console.log(error));
-      alert("The file is successfully updated");
       [
         refTitle.current.value,
         refDescription.current.value,
@@ -257,6 +287,12 @@ function AddProperty() {
       setBathrooms(1);
       setBeds(1);
       setBedrooms(1);
+      Swal.fire("Edited!", "", "success");
+          // setTimeout(dispatchuser, 2000);
+        } else if (result.isDenied) {
+          Swal.fire("Property not edited", "", "info");
+        }
+      });
     }
   };
 
@@ -661,6 +697,7 @@ function AddProperty() {
           flexDirection: "column",
         }}
       >
+<<<<<<< HEAD
         {idParam.id && (
           <div className="calendary-center">
             <h2>AVAILABILITY</h2>
@@ -700,6 +737,41 @@ function AddProperty() {
               </button>
               <SimpleModal data={fechasModal} idProp={idParam.id} />
             </div>
+=======
+        <div className="calendary-center">
+          <h2>AVAILABILITY</h2>
+          <p style={{ width: "60%", margin: "0", textAlign: "match-parent" }}>
+            Select the dates you don´t want to keep available for booking. Dates
+            you dont´t select will be available for renting but don´t worry, you
+            can always changes your selections{" "}
+          </p>
+          <div className="calendary">
+            <DatePicker
+              excludeDates={disableFinal}
+              minDate={new Date()}
+              startDate={startDate}
+              endDate={endDate}
+              selectsRange
+              selected={startDate}
+              onChange={onChange}
+              inline
+              monthsShown={2}
+              filterDate={disable ? isWeekday : false}
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              gap: "5px",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <button className="btn-days" onClick={() => dispatchDates()}>
+              Block selected dates{" "}
+            </button>
+            <SimpleModal data={fechasModal} idProp={idParam.id} />
+>>>>>>> f3ab612ddf16e2d58aab3aca43f0faa02af6cbb7
           </div>
         )}
         {!idParam.id && (
