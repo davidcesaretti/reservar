aaeec00979ea9e999a9ced754f92b48276a31f0c
jesusegg <jesusegg@hotmail.com>
2021-08-05 22:50:52 -0500

    mediaApp

--- front-end/src/components/MediaQuery/Media.tsx
+++ front-end/src/components/MediaQuery/Media.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Media() {
-  return (
-    <div>
-      <p>For while only disponible in desktop</p>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Media;
